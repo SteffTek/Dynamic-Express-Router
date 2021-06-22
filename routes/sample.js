f63b2e6d300572b393584c342862a25007468149
SteffTek <spfahler@t-online.de>
2021-06-22 07:15:34 +0200

    Create sample.js

---
+++ routes/sample.js
@@ -0,0 +1,13 @@
+// API PATH
+const path = "/api/lel/lul";
+
+// API EXECUTION
+const router = (req, res) => {
+    console.log("LOL");
+}
+
+// EXPOSE TO ROUTER
+module.exports = {
+    path: path,
+    router: router
+}
